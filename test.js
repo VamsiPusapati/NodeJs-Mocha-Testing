23b42c6736ffd5e604f39f668dfb6eb5fbde5ecb
VamsiPusapati <200306175@student.georgianc.on.ca>
2017-04-12 15:32:58 -0400

    Commented the code

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ var assert = require('assert');
 
 var calc = require('./calc.js');
 
+// 'describe' creates a suite of test cases.
+// 'it' implements the test cases.
+
+// Mocha passes a done object. done should called whenever the test case is finished.
+// We can embed a describe in another describe. 
 describe('Calculator Tests', function() {
 	describe('Addition Tests', function() {
 		it('returns 1 + 1 = 2', function(done) {
